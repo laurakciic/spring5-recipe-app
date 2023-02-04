ad8b1af20733e19a8a31c1b70235f8b611922051
Laura <laurakovacic@outlook.com>
2023-02-04 10:30:47 +0100

    initial commit

---
+++ src/test/java/com/laurakovacic/spring5recipeapp/Spring5RecipeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laurakovacic.spring5recipeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5RecipeAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
