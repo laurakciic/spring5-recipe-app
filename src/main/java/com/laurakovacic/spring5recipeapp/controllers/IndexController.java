191ec3de6e97506d99ad337a72b2aaac253b9b2d
Laura <laurakovacic@outlook.com>
2023-02-11 09:34:30 +0100

    demonstrate Spring Data JPA query methods

--- src/main/java/com/laurakovacic/spring5recipeapp/controllers/IndexController.java
+++ src/main/java/com/laurakovacic/spring5recipeapp/controllers/IndexController.java
@@ -1,13 +1,33 @@
 package com.laurakovacic.spring5recipeapp.controllers;
 
+import com.laurakovacic.spring5recipeapp.model.Category;
+import com.laurakovacic.spring5recipeapp.model.UnitOfMeasure;
+import com.laurakovacic.spring5recipeapp.repositories.CategoryRepository;
+import com.laurakovacic.spring5recipeapp.repositories.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Optional;
+
 @Controller
 public class IndexController {
+    private CategoryRepository categoryRepository;
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
 
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage() {
+
+        Optional<Category> categoryOptional = categoryRepository.findByDescription("Japanese");
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+        System.out.println("Category ID: " + categoryOptional.get().getId());
+        System.out.println("UOM ID: " + unitOfMeasureOptional.get().getId());
+
         return "index";
     }
 }
--- src/main/java/com/laurakovacic/spring5recipeapp/repositories/UnitOfMeasureRepository.java
+++ src/main/java/com/laurakovacic/spring5recipeapp/repositories/UnitOfMeasureRepository.java
@@ -3,5 +3,8 @@ package com.laurakovacic.spring5recipeapp.repositories;
 import com.laurakovacic.spring5recipeapp.model.UnitOfMeasure;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+    Optional<UnitOfMeasure> findByDescription(String description);
 }
