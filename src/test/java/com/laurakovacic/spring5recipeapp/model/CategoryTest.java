803408a9229e5b748ae59068ca94450d85ad6de8
Laura <laurakovacic@outlook.com>
2023-02-16 17:35:59 +0100

    added a JUnit test

---
+++ src/test/java/com/laurakovacic/spring5recipeapp/model/CategoryTest.java
@@ -0,0 +1,32 @@
+package com.laurakovacic.spring5recipeapp.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CategoryTest {
+    Category category;
+
+    @BeforeEach
+    public void setCategory() {
+        category = new Category();
+    }
+
+    @Test
+    void getId() {
+        Long idValue = 4L;
+
+        category.setId(idValue);
+
+        assertEquals(idValue, category.getId());
+    }
+
+    @Test
+    void getDescription() {
+    }
+
+    @Test
+    void getRecipes() {
+    }
+}
