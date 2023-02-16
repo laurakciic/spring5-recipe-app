e0653a3be2dfd8495bb4cf29a4be1ebbb937db74
Laura <laurakovacic@outlook.com>
2023-02-16 19:34:15 +0100

    implement IndexControllerTest with mockito argument capture

--- src/test/java/com/laurakovacic/spring5recipeapp/controllers/IndexControllerTest.java
+++ src/test/java/com/laurakovacic/spring5recipeapp/controllers/IndexControllerTest.java
@@ -1,11 +1,16 @@
 package com.laurakovacic.spring5recipeapp.controllers;
 
+import com.laurakovacic.spring5recipeapp.model.Recipe;
 import com.laurakovacic.spring5recipeapp.services.RecipeService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
+
+import java.util.HashSet;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -28,11 +33,27 @@ class IndexControllerTest {
 
     @Test
     void getIndexPage() {
+        // given
+        Set<Recipe> recipes = new HashSet<>();
+        recipes.add(new Recipe());
+
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+
+        recipes.add(recipe);
+
+        when(recipeService.getRecipes()).thenReturn(recipes);
+
+        ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
+
+        // when
         String viewName = indexController.getIndexPage(model);
         assertEquals("index", viewName);
 
-        // expecting to add attribute named "recipes" with anySet() value
-        verify(model, times(1)).addAttribute(eq("recipes"), anySet());
+        // then
         verify(recipeService, times(1)).getRecipes();
+        verify(model, times(1)).addAttribute(eq("recipes"), argumentCaptor.capture());
+        Set<Recipe> setInController = argumentCaptor.getValue();
+        assertEquals(2, setInController.size());
     }
 }
