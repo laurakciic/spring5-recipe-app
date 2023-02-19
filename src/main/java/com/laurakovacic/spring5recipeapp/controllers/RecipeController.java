03fa4ded66c7442e5f0e3f596b9cb82d16cd8ce4
Laura <laurakovacic@outlook.com>
2023-02-19 17:12:26 +0100

    added form for creating a new recipe

--- src/main/java/com/laurakovacic/spring5recipeapp/controllers/RecipeController.java
+++ src/main/java/com/laurakovacic/spring5recipeapp/controllers/RecipeController.java
@@ -1,10 +1,10 @@
 package com.laurakovacic.spring5recipeapp.controllers;
 
+import com.laurakovacic.spring5recipeapp.commands.RecipeCommand;
 import com.laurakovacic.spring5recipeapp.services.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class RecipeController {
@@ -20,5 +20,21 @@ public class RecipeController {
         model.addAttribute("recipe", recipeService.findById(Long.valueOf(id)));
 
         return "recipe/show";
+    }
+
+    @RequestMapping("recipe/new")
+    public String newRecipe(Model model) {
+
+        model.addAttribute("recipe", new RecipeCommand());
+
+        return "recipe/recipeform";
+    }
+
+    @PostMapping     // when we get POST to the recipe url/recipe path it will invoke this method
+    @RequestMapping("recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand command) {     // bind form post params to RecipeCommand obj
+        RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
+
+        return "redirect:/recipe/show/" + savedCommand.getId();
     }
 }
